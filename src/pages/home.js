80da99225f4c993cc613bb13b19b7f4ee7cc2e5a
naaatii <51373713+naaatii@users.noreply.github.com>
2020-08-26 23:14:25 -0300

    tuxson

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+/*import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";*/
 import cero from '../assets/0.png'
 import uno from '../assets/1.png'
 import dos from '../assets/2.png'
 import tres from '../assets/3.png'
 import cuatro from '../assets/4.png'
 
- const Home = () => {
-  //render() {
+export default class Home extends Component {
+  render() {
     const settings = {
       dots: false,
       arrows: true,
@@ -19,36 +19,33 @@ import cuatro from '../assets/4.png'
       speed: 1000,
       slidesToShow: 1,
       slidesToScroll: 1,
-      
     };
-      
-    //};
 
     return (
-      <div>
+
         <Slider {...settings}>
           <div className="contenedorImgSlider">
             <img alt="0.png" src={cero}/>
           </div>
 
-          <div>
+          <div className="contenedorImgSlider">
             <img alt="1.png" src={uno}/>
           </div>
 
-          <div>
+          <div className="contenedorImgSlider">
             <img alt="2.png" src={dos}/>
           </div>
           
-          <div>
+          <div className="contenedorImgSlider">
             <img  alt="3.png" src={tres}/>
           </div>
 
-          <div>
+          <div className="contenedorImgSlider">
             <img alt="4.png" src={cuatro}/>
           </div>
         </Slider>
-      </div>
+
     );
   }
-
-export default Home
+}
+//export default Home
